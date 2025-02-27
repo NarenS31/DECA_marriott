881d97e66dd4dd92bbcdeeb05f758b74aeb99523
NarenS31 <127507998+NarenS31@users.noreply.github.com>
2025-02-26 21:23:08 -0500

    initial code

---
+++ marriottappTests/marriottappTests.swift
@@ -0,0 +1,16 @@
+//
+//  marriottappTests.swift
+//  marriottappTests
+//
+//  Created by naren sara on 2/25/25.
+//
+
+import Testing
+
+struct marriottappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
